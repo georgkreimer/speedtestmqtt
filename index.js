1f50966f76b588603ca4b4b7c0537e3bfec02e58
Georg Kreimer <georg.kreimer@sap.com>
2017-06-08 20:34:20 +0200

    log up/downstream and publish via mqtt

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const speedTest = require('speedtest-net');
-const test = speedTest({maxTime: 5000});
+const test = speedTest({maxTime: 60000});
 const mqtt = require('mqtt');
 
 const {url, credentials, topic} = require('./config/mqtt');
@@ -8,10 +8,19 @@ const client = mqtt.connect(url, credentials);
 client.on('connect', function() {
 	console.log('connected to MQTT server');
 
-// run speed test
+  test.on('data', data => {
+    const client = mqtt.connect(url, credentials);
 
-	const topic = 'topic';
-  client.publish(topic, "value");
+    console.log('down: ', data.speeds.download, 'up: ', data.speeds.upload);
+
+    const upstreamTopic = `${topic}/bandwidth/upstream`;
+  	const downstreamTopic = `${topic}/bandwidth/downstream`;
+
+    client.publish(upstreamTopic, data.speeds.download);
+    client.publish(downstreamTopic, data.speeds.upload);
+  });
+
+  test.on('error', console.error);
 });
 
 client.on('error', console.error);
